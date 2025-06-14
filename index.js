d2e9a09d1d396f35ecaa1053e212a91b3f4d6a46
ann ngatia <annngatia94@gmail.com>
2025-06-14 14:36:46 +0300

    Moving elements on the page

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+function moveDodgerRight() {
+  const dodger = document.getElementById("dodger");
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (event.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
+
